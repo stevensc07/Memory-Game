1588e785050c88cb43fc86e37e8934d7c27df9d3
Carlos Andrés Sánchez <andresagudelo9239@gmail.com>
2020-08-13 16:59:26 -0500

    ScreenOrientation locked. Create list with random number for image position

--- app/src/main/java/com/cagudeloa/memorygame/MainActivity.kt
+++ app/src/main/java/com/cagudeloa/memorygame/MainActivity.kt
@@ -2,8 +2,11 @@ package com.cagudeloa.memorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.AdapterView
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.cagudeloa.memorygame.databinding.ActivityMainBinding
@@ -11,25 +14,22 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var secondsSelected: Int = 3
+    lateinit var binding: ActivityMainBinding
+    lateinit var mainButton: ImageView
+    val listNumbers: MutableList<Int> = (1..12).toMutableList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,  R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,  R.layout.activity_main)
 
-        spinnerFunction()
+        binding.mainButton.setOnClickListener {
+            getRandomPairs()
+        }
+
     }
 
-    private fun spinnerFunction() {
-        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(adapterView: AdapterView<*>?) {}
-
-            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                secondsSelected = adapterView!!.getItemAtPosition(position).toString().toInt()
-                Toast.makeText(this@MainActivity,   // Like this bc we're inside an anonymous function
-                    "You selected $secondsSelected seconds",
-                    Toast.LENGTH_LONG).show()
-            }
-        }
+    private fun getRandomPairs() {
+        listNumbers.shuffle()
+        //Log.d("test", listNumbers.toString())
     }
 }
